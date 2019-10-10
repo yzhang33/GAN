554fce3ce50642ae115631d6f64fa17d933af16e
yzhang33 <youmingzhang19@gmail.com>
2019-10-09 22:32:18 -0400

    GAN completed

--- dcgan.py
+++ dcgan.py
@@ -52,10 +52,82 @@ class G(nn.Module):
                 nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
                 nn.Tanh()
             )
-        def forward(self, input):
-            output = self.main(input)
-            return output
+    def forward(self, input):
+        output = self.main(input)
+        return output
         
 # create generator
 netG = G()
 netG.apply(weights_init)
+
+class D(nn.Module):
+    def __init__(self):
+        super(D,self).__init__()
+        self.main = nn.Sequential(
+                nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(128),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Conv2d(128,256, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(256),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Conv2d(256,512, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(512),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Conv2d(512,1, 4, 1, 0, bias=False),
+                nn.Sigmoid()
+        )
+    def forward(self,input):
+        output = self.main(input)
+        return output.view(-1) #flatern to 2d
+        
+# create discriminator
+netD = D()
+netD.apply(weights_init)
+
+# Train G and D
+criterion = nn.BCELoss()
+optimizerD = optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5,0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5,0.999))
+
+for epoch in range(25):
+    for i, data in enumerate(dataloader, 0):
+        # update weight of D network
+        netD.zero_grad()
+        real, _ = data
+        input = Variable(real)
+        target = Variable(torch.ones(input.size()[0]))
+        output = netD(input)
+        errorD_real = criterion(output, target)
+        
+        # train D with fake images
+        noise = Variable(torch.randn(input.size()[0], 100, 1, 1))
+        fake = netG(noise)  #fake image generated
+        target = Variable(torch.zeros(input.size()[0]))
+        output = netD(fake.detach())
+        errorD_fake = criterion(output, target)
+
+        errorD = errorD_real + errorD_fake
+        errorD.backward()
+        optimizerD.step()
+        
+        # update weights of G to get close to 1 
+        netG.zero_grad()
+        target = Variable(torch.ones(input.size()[0]))
+        output = netD(fake)
+        errorG = criterion(output, target)
+        errorG.backward()
+        optimizerG.step()
+        
+        print('[%d/%d][%d/%d] loss_D: %.4f loss_G: %.4f' %(epoch, 25, i, len(dataloader), errorD.data[0], errorG.data[0]))
+        
+        if i % 100 == 0:
+            vutils.save_image(real,'%s/real_samples.png' % "./result", normalize=True)
+            fake = netG(noise)
+            vutils.save_image(fake.data,'%s/fake_samples_epoch_%03d.png' % ("./result", epoch),normalize=True)
+
+        
+        
+        
+        
